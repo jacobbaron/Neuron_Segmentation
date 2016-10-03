770554f1985fef52df5b0394cec61b621c584bbd
jacobbaron <jbaron@physics.harvard.edu>
2016-10-03 10:22:15 -0400

    some unknown change...

--- Compiled/add2TrainingDataset.m
+++ Compiled/add2TrainingDataset.m
@@ -1,4 +1,4 @@
-function add2TrainingDataset(nmPeakSigNew,filenameNew,labelsNew)
+function add2TrainingDataset(nmPeakSigNew,filenameNew,labelsNew,neuronIdx)
     [~,date,~]=fileparts(pwd);
     runidx=strfind(filenameNew,'run')+3;
     dotidx=strfind(filenameNew,'.nd2')-1;
@@ -6,20 +6,44 @@ function add2TrainingDataset(nmPeakSigNew,filenameNew,labelsNew)
     expt=rem(str2double(filenameNew(runidx:dotidx)),animal*100);
     neuronsListNew=[str2double(date)*ones(length(labelsNew),1),...
         animal*ones(length(labelsNew),1),expt*ones(length(labelsNew),1),...
-        (1:length(labelsNew))'];
+        neuronIdx'];
     
     compiled_data_path=which('compiled_data.mat');
     compiled_data_folder=fileparts(compiled_data_path);
     C=load('compiled_data.mat');
-    last_animal=sprintf('%0.0f_%0.0f',C.neuronList(end,1),...
-        C.neuronList(end,2)*100+C.neuronList(end,3));
-    backupfilename=sprintf('compiled_data_backup%s.mat',last_animal);
-    save(fullfile(compiled_data_folder,'Compiled_data_backup',backupfilename),...
-        '-struct','C');
-    C.neuronList=[C.neuronList;neuronsListNew];
-    C.labels=[C.labels;labelsNew'];
-    C.nmPeakSig=cat(3,C.nmPeakSig,nmPeakSigNew);
-    [C.nmSigList,C.odorsList]=dispNeuronSignals(C.nmPeakSig);
-    save(compiled_data_path,'-struct','C');
-    
+    added=any(ismember(C.neuronList(:,1:3),neuronsListNew(:,1:3),'rows'));
+    if added
+        replace = questdlg('This experiment has been added already. Replace?',...
+            'Replace?','Replace','Do nothing','Do nothing');
+        
+    else
+        replace = 'Replace';
+    end
+    switch replace
+        case 'Replace'
+            %save backup
+            if ~all(size(C.nmPeakSig(:,:,1))==size(nmPeakSigNew(:,:,1)))
+                
+            end
+            last_animal=sprintf('%0.0f_%0.0f',C.neuronList(end,1),...
+                C.neuronList(end,2)*100+C.neuronList(end,3));
+            backupfilename=sprintf('compiled_data_backup%s.mat',last_animal);
+            save(fullfile(compiled_data_folder,'Compiled_data_backup',backupfilename),...
+                '-struct','C');
+            
+            %delete existing data from this experiment.
+            items2delete=ismember(C.neuronList(:,1:3),neuronsListNew(1,1:3),'rows');
+            C.labels(items2delete)=[];
+            C.neuronList(items2delete,:)=[];
+            C.nmPeakSig(:,:,items2delete)=[];
+            C.nmSigList(items2delete)=[];
+            
+            
+            
+            C.neuronList=[C.neuronList;neuronsListNew];
+            C.labels=[C.labels;labelsNew'];
+            C.nmPeakSig=cat(3,C.nmPeakSig,nmPeakSigNew);
+            [C.nmSigList,C.odorsList]=dispNeuronSignals(C.nmPeakSig);
+            save(compiled_data_path,'-struct','C');
+    end    
     
